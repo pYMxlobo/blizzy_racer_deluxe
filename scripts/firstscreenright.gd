aa282d8ebe916160c3658fbb6accd00511654144
pYMxolbo <pymxolboart@gmail.com>
2024-04-17 18:01:51 -0400

    new thing

---
+++ scripts/firstscreenright.gd
@@ -0,0 +1,16 @@
+extends TextureButton
+
+
+
+@onready var next = preload("res://scenes/mapselectpage2.tscn")
+
+#
+func surebuddy(skibidi):
+		position.x = skibidi
+		position.y = skibidi
+
+
+
+func _on_pressed():
+	surebuddy(0)
+	get_tree().change_scene_to_packed(next)
---
+++ scripts/towerpageleft.gd
@@ -0,0 +1,14 @@
+extends TextureButton
+
+@onready var next = preload("res://scenes/map_select.tscn")
+
+#
+func surebuddy(skibidi):
+		position.x = skibidi
+		position.y = skibidi
+
+
+
+func _on_pressed():
+	surebuddy(0)
+	get_tree().change_scene_to_packed(next)
